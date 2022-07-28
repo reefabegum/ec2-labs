6b2f98084d0369145e231d96aa33fb75d00cdf48
reefabegum <reefa.begum@cloudreach.com>
2022-07-28 09:35:29 +0530

    modified the files again

--- security-groups.tf
+++ security-groups.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "ansible_server" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["157.45.242.138/32"]
+    cidr_blocks      = ["157.45.138.253/32"]
     #ipv6_cidr_blocks = ["2409:4071:d07:fcdc:94b6:d54:411:1b1/64"]
     
   }
@@ -17,19 +17,11 @@ ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["157.45.242.138/32"]
+    cidr_blocks      = ["157.45.138.253/32"]
     #ipv6_cidr_blocks = ["2409:4071:d07:fcdc:94b6:d54:411:1b1/64"]
     
   }
-  ingress {
-    description      = "Allow port 3306"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = ["157.45.242.138/32"]
-    #ipv6_cidr_blocks = ["2409:4071:d07:fcdc:94b6:d54:411:1b1/64"]
-    
-  }
+  
   egress {
     from_port        = 0
     to_port          = 0
@@ -39,6 +31,6 @@ ingress {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "ansible_server"
   }
 }
